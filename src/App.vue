0501eecb3bd23bfa2998483e185829d0e0c7d6f5
Omar Hasan <omar.hassan3030@gmail.com>
2022-05-29 19:46:58 +0300

    Publish currrent task

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,196 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Table Summary  -->
+    <ul id="table-summary">
+      <li><span>total</span> {{ notes.length }}</li>
+      <li><span>completed</span>{{ getNotesCount("completed") }}</li>
+      <li><span>not completed</span>{{ getNotesCount("not_completed") }}</li>
+    </ul>
+    <div></div>
+    <!-- Notes Table -->
+    <div id="notes-table">
+      <table>
+        <thead>
+          <tr>
+            <th v-for="(th, index) in table_th" :key="th">
+              <input
+                @click="selectAllNotes()"
+                v-if="index == 0"
+                type="checkbox"
+                :checked="
+                  selectedNotes.length === notes.length && selectedNotes.length
+                "
+              />{{ th }}
+              <div class="sort-icons" @click="sortNotes(th.toLowerCase())">
+                <span
+                  :class="{ active: isActiveSortBtn(th.toLowerCase(), -1) }"
+                ></span>
+                <span
+                  :class="{ active: isActiveSortBtn(th.toLowerCase(), 1) }"
+                ></span>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="note in notes" :key="note.id">
+            <td>
+              <label :for="note.id"
+                ><input
+                  :id="note.id"
+                  :value="note.id"
+                  v-model="selectedNotes"
+                  type="checkbox"
+                />
+                {{ note.id }}</label
+              >
+            </td>
+            <td>{{ note.title }}</td>
+            <td>{{ note.content }}</td>
+            <td>{{ note.status }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <button class="filled" id="notes-table__add" @click="openNotesDialog()">
+        Add
+      </button>
+    </div>
+    <!-- Action Panel -->
+    <div id="action-panel" v-if="selectedNotes.length">
+      <p>
+        <img :src="alertIcon" alt="" /> Do you want to delete
+        {{ selectedNotes.length > 1 ? "these notes" : "this note" }}?
+      </p>
+      <div id="action-panel__controls">
+        <button @click="selectedNotes = []">No</button>
+        <button
+          class="filled"
+          @click="
+            removeNotes();
+            selectedNotes = [];
+          "
+        >
+          Yes
+        </button>
+      </div>
+    </div>
+    <!-- Add Note Dialog -->
+    <form
+      id="note-dialog"
+      v-if="showNotesDialog"
+      @keyup.enter="isFormValid() ? saveNote() : ''"
+    >
+      {{ newNote }}
+      <h4>Add note</h4>
+      <input
+        type="text"
+        placeholder="Add title"
+        v-model="newNote.title"
+        ref="noteTitleRef"
+      />
+      <textarea placeholder="Add Content" v-model="newNote.content"></textarea>
+      <div id="note-dialog__controls">
+        <button class="filled" @click="saveNote()" :disabled="!isFormValid()">
+          Save
+        </button>
+        <button
+          @click="
+            showNotesDialog = false;
+            resetForm();
+          "
+        >
+          Cancel
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import alertIcon from "./assets/alert.svg";
+import notes from "./assets/data.json";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      showNotesDialog: false,
+      selectedNotes: [],
+      newNote: {},
+      sortBtn: {
+        key: "",
+        order: -1,
+      },
+      alertIcon,
+      notes,
+      latestNoteId: 0,
+      table_th: ["ID", "Title", "Content", "Status"],
+    };
+  },
+  methods: {
+    openNotesDialog() {
+      this.showNotesDialog = true;
+      setTimeout(() => {
+        this.$refs.noteTitleRef.focus();
+      }, 100);
+    },
+    resetForm() {
+      this.newNote = {
+        id: this.latestNoteId + 1,
+        title: "",
+        content: "",
+        status: "New",
+      };
+      this.showNotesDialog = false;
+    },
+    saveNote() {
+      this.notes.push(this.newNote);
+      this.latestNoteId++;
+      this.resetForm();
+    },
+    isFormValid() {
+      return !!this.newNote.title && !!this.newNote.content.trim();
+    },
+    getNotesCount(type) {
+      return this.notes.filter(
+        (note) => note.status.toLowerCase() === type.replace("_", " ")
+      ).length;
+    },
+    sortNotes(sortBy) {
+      if (this.sortBtn.key === sortBy) {
+        // Same Filter
+        this.sortBtn.order = -1 * this.sortBtn.order;
+      } else {
+        this.sortBtn.order = -1;
+      }
+      this.sortBtn.key = sortBy;
+
+      this.notes = this.notes.sort((a, b) =>
+        a[sortBy] < b[sortBy] ? 1 * this.sortBtn.order : -1 * this.sortBtn.order
+      );
+    },
+    selectAllNotes() {
+      this.selectedNotes =
+        this.selectedNotes.length === this.notes.length
+          ? []
+          : this.notes.map((note) => note.id);
+    },
+    isActiveSortBtn(key, order) {
+      return this.sortBtn.key === key && this.sortBtn.order === order;
+    },
+    removeNotes() {
+      this.notes = this.notes.filter(
+        (note) => !this.selectedNotes.includes(note.id)
+      );
+    },
+  },
+  mounted() {
+    this.latestNoteId = this.notes.length;
+    this.resetForm();
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap");
+@import "./App.scss";
 </style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,41 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import App from "@/App";
+import { mount } from "@vue/test-utils";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+describe("Add notes", () => {
+  it("Open add note panel", async () => {
+    const wrapper = mount(App);
+    expect(wrapper.find("#note-dialog").exists()).toBe(false);
+
+    const addNoteBtn = wrapper.find("#notes-table__add");
+    expect(addNoteBtn).toBeDefined();
+    await addNoteBtn.trigger("click");
+    expect(wrapper.find("#note-dialog").exists()).toBe(true);
+  });
+
+  it("Add note form validation", async () => {
+    const wrapper = mount(App);
+
+    const addNoteBtn = wrapper.find("#notes-table__add");
+    await addNoteBtn.trigger("click");
+    expect(
+      wrapper.find("#note-dialog__controls button").attributes("disabled")
+    ).toBe("disabled");
+    await wrapper.find("#note-dialog input").setValue("test title");
+    await wrapper.find("#note-dialog textarea").setValue("test content");
+    expect(
+      wrapper.find("#note-dialog__controls button").attributes("disabled")
+    ).toBe(undefined);
+  });
+
+  it("Add note", async () => {
+    const wrapper = mount(App);
+
+    const addNoteBtn = wrapper.find("#notes-table__add");
+    await addNoteBtn.trigger("click");
+    wrapper.find("#note-dialog input").setValue("test title");
+    await wrapper.find("#note-dialog textarea").setValue("test content");
+    const saveNote = wrapper.find("#note-dialog__controls button.filled");
+    saveNote.trigger("click");
+    expect(wrapper.vm.$data.notes.length).toBe(4);
+  });
+});
